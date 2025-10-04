f05994cf79a9234bda0538efd0b4e097c13a1b1f
Your Name <you@example.com>
2025-10-04 14:09:16 +0530

    Implement Auth

---
+++ src/utils/format.js
@@ -0,0 +1,8 @@
+export const formatValidationError = (errors) => {
+  console.log(JSON.stringify(errors));
+  if(!errors || !errors.issues) return 'validation failed';
+
+  if(Array.isArray(errors.issues)) return errors.issues.map(i => i.message).join(',');
+
+  return JSON.stringify(errors); 
+};
